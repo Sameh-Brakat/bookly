4ed619bb85f66bafdc9a736eeb98f274383b7822
Sameh-Brakat <sameh13652@gmail.com>
2024-02-22 11:48:44 +0200

    add BlocBuilder to FeaturedBooksListView

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/Features/home/presentation/controllers/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/controllers/featured_books_cubit/featured_books_states.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'best_seller_list_view.dart';
 import 'best_seller_list_view_item.dart';
@@ -13,18 +16,18 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverToBoxAdapter(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
+            children: [
               Padding(
                   padding: EdgeInsets.symmetric(
                     horizontal: 30,
                   ),
                   child: CustomAppBar()),
-              FeaturedBooksListView(),
+              FeaturedBooksListViewBuilder(),
               SizedBox(
                 height: 50,
               ),
@@ -41,13 +44,34 @@ class HomeViewBody extends StatelessWidget {
             ],
           ),
         ),
-        const SliverFillRemaining(
+        SliverFillRemaining(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: BestSellerListView(),
           ),
         ),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListViewBuilder extends StatelessWidget {
+  const FeaturedBooksListViewBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksStates>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return const FeaturedBooksListView();
+        } else if (state is FeaturedBooksError) {
+          return Text(state.message);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
