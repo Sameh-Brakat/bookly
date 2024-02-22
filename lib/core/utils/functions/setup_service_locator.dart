dcfc9a0581b8a794ff536d0839a54e0e464fe085
Sameh-Brakat <sameh13652@gmail.com>
2024-02-22 09:45:06 +0200

    setup service locator

---
+++ lib/core/utils/functions/setup_service_locator.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly/Features/home/data/repositories/home_repository_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<HomeRepositoryImpl>(
+    HomeRepositoryImpl(
+      homeLocalDataSource: HomeLocalDataSourceImp(),
+      homeRemoteDataSource: HomeRemoteDataSourceImp(),
+    ),
+  );
+}
